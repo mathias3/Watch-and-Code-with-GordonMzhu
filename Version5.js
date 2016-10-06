838854fbc20f249f0a9abb8a4024211b811b0e29
mathias3 <maciej.osowski@gmail.com>
2016-10-07 00:31:44 +0100

    Update Version5.js

--- Version5.js
+++ Version5.js
@@ -1,9 +1,20 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log("My Todos:");
-    for (var i = 0; i < this.todos.length; i++) {
-      console.log(this.todos[i].todoText);
+ if (this.todos.length === 0) {
+      console.log("Your todo list is empty")
+    } else {
+      console.log("My Todos:");
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+
+        if (this.todos[i].completed === true) {
+          console.log("(x)",this.todos[i].todoText);
+        } else {
+          console.log("( )",this.todos[i].todoText);
+        }
+      }
+
     }
   },
   addTodo: function(todoText) {
