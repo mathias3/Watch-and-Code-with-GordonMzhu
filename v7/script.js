b6a38f6fe15b0bb9f99fa9f2a8acfe1e823b5dd6
mathias3 <maciej.osowski@gmail.com>
2016-10-18 23:50:29 +0100

    Update script.js

--- v7/script.js
+++ v7/script.js
@@ -62,4 +62,12 @@ var todoList = {
   }
 
 };
+var displayTodosButton = document.getElementById("displayTodosButton");
+var toggleAllButton = document.getElementById("toggleAllButton");
 
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+});
