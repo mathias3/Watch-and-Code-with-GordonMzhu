4b83b3e1e139d3b2a87fd46695feb1a0d54a3a5c
mathias3 <maciej.osowski@gmail.com>
2016-10-31 22:57:56 +0000

    Update script.js

--- v9/script.js
+++ v9/script.js
@@ -100,8 +100,8 @@ var view = {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
     for (var i = 0; i < todoList.todos.length; i++) {
-
       var todoLi = document.createElement('li');
+      todoLi.textContent = todoList.todos[i].todoText;
       todosUl.appendChild(todoLi);
     }
   }
